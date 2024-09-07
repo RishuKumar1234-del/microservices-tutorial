3e2e32a2c8cdd5d51a71440136e2dd26176d30aa
rishu kumar <kumarrishu1804@gmail.com>
2024-09-07 20:16:28 +0530

    Rating service as second commit

---
+++ RatingServices/src/main/java/com/lcwd/rating/RatingServicesApplication.java
@@ -0,0 +1,15 @@
+package com.lcwd.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RatingServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingServicesApplication.class, args);
+		System.out.println("RatingService Started!!!");
+	}
+
+}
